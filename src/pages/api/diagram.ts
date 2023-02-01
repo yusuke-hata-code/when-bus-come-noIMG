999ce4b11d4bcd285da3892bfcbb13040b8fb257
yusuke-hata-code <yuusuke0972@gmail.com>
2023-02-01 18:51:20 +0900

    フロントでサンプルが動くようになった

---
+++ src/pages/api/diagram.ts
@@ -0,0 +1,68 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next';
+import { Temporal } from '@js-temporal/polyfill';
+import { JSDOM } from 'jsdom';
+
+// /api/diagram?dest=tonda
+// /api/diagram?dest=takatsuki
+
+type Diagram = number[];
+
+const getUrl = (type: 'takatsuki' | 'tonda' | string): string | null => {
+  const baseUrl =
+    'https://transfer.navitime.biz/takatsuki/smart/diagram/Search/bus';
+  const datetime = `${Temporal.Now.plainDateISO().toString()}T03:00+09:00`;
+  const paramObj = {
+    takatsuki: {
+      datetime,
+      startId: '00150093',
+      stopNo: '1',
+      course: '0003700096',
+    },
+    tonda: {
+      datetime,
+      startId: '00150093',
+      stopNo: '8',
+      course: '0003700142',
+    },
+  } as const;
+
+  if (type === 'takatsuki' || type === 'tonda') {
+    return `${baseUrl}?${new URLSearchParams(paramObj[type]).toString()}`;
+  }
+
+  return null;
+};
+
+const getDiagram = async (url: string): Promise<Diagram> => {
+  const text = await (await fetch(url)).text();
+  const dateString = Temporal.Now.plainDateISO().toString();
+  const document = new JSDOM(text).window.document;
+
+  return [
+    ...document.querySelectorAll('div[style="display:block"] .hour-frame'),
+  ].flatMap((e) => {
+    const hour = `0${e.getAttribute('value')}`.slice(-2);
+    return [...e.querySelectorAll('.minute>span[aria-hidden="true"]')].map(
+      (e) => new Date(`${dateString}T${hour}:${e.textContent}+09:00`).getTime()
+    );
+  });
+};
+
+const handler = (req: NextApiRequest, res: NextApiResponse<Diagram>) => {
+  if (typeof req.query.dest === 'string') {
+    const url = getUrl(req.query.dest);
+    console.log(url);
+    if (url) {
+      return getDiagram(url)
+        .then((diagram) => {
+          console.log(diagram);
+          res.status(200).json(diagram);
+        })
+        .catch(() => res.status(404).json([]));
+    }
+  }
+  return res.status(404).json([]);
+};
+
+export default handler;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,133 @@
 import Head from 'next/head';
+import React, { useState, useEffect } from 'react';
+let sampleDiagram: number[] = [
+  1675200900000, 1675202460000, 1675205040000, 1675206600000, 1675210680000,
+  1675212300000, 1675215600000, 1675216980000, 1675219200000, 1675222740000,
+  1675224420000, 1675226580000, 1675230060000, 1675232400000, 1675236660000,
+  1675237740000, 1675240680000, 1675242060000, 1675242600000, 1675247760000,
+  1675250160000, 1675254600000, 1675258260000,
+].sort();
 
+let sampleRenderingDiagram = [
+  {
+    time: '19:36',
+    limit: 14,
+  },
+];
+
+//10:10,10:30,10:45,11:30,12:00
 export default function Home() {
+  const [diagram, setDiagram] = useState<diagramObj[]>(sampleRenderingDiagram);
+  useEffect(() => {
+    const timer = setInterval(() => {
+      setDiagram(filterDiagramArr({ diagramArr: sampleDiagram })), 1000;
+    });
+    return () => clearInterval(timer);
+  }, []);
   return (
     <>
       <Head>
         <title>when-bus-come</title>
       </Head>
-      <h1>Hi</h1>
+      <div>
+        <table border={1}>
+          <tbody>
+            <tr>
+              <th colSpan={3}>高槻行き</th>
+            </tr>
+            <tr>
+              <td>時刻</td>
+              <td>残り</td>
+            </tr>
+            {diagram.map((diagram) => {
+              return (
+                <>
+                  <tr>
+                    <td>{diagram.time}</td>
+                    <td>{`${Math.trunc(diagram.limit / 60)}分${
+                      diagram.limit % 60
+                    }秒`}</td>
+                  </tr>
+                </>
+              );
+            })}
+          </tbody>
+        </table>
+
+        <table border={1}>
+          <tbody>
+            <tr>
+              <th colSpan={3}>富田行き</th>
+            </tr>
+            <tr>
+              <td>時刻</td>
+              <td>残り</td>
+            </tr>
+            {diagram.map((diagram) => {
+              return (
+                <>
+                  <tr>
+                    <td>{diagram.time}</td>
+                    <td>{`${Math.trunc(diagram.limit / 60)}分${
+                      diagram.limit % 60
+                    }秒`}</td>
+                  </tr>
+                </>
+              );
+            })}
+          </tbody>
+        </table>
+      </div>
     </>
   );
 }
+type diagramObj = {
+  time: string;
+  limit: number;
+};
+
+type DiagramArr = number[];
+
+const filterDiagramArr = ({ diagramArr }: { diagramArr: DiagramArr }) => {
+  const currentTime = new Date().getTime();
+  const filteredDiagramArr = diagramArr
+    .filter((diagram) => {
+      return diagram >= currentTime + 120 * 1000;
+    })
+    .filter((_, i) => {
+      console.log(_);
+      return i < 3;
+    });
+
+  const limitTimeTable = limitMinute({ diagramArr: filteredDiagramArr });
+  const timeTable = unixTimeToViewTime({ diagramArr: filteredDiagramArr });
+  return timeTable.map((time, i) => {
+    return { time: time, limit: limitTimeTable[i] };
+  });
+};
+
+const unixTimeToViewTime = ({
+  diagramArr,
+}: {
+  diagramArr: DiagramArr;
+}): string[] => {
+  return diagramArr.map((diagram) => {
+    const hour = new Date(diagram).getHours();
+    const minute = String(new Date(diagram).getMinutes()).padStart(2, '0');
+    return `${hour}:${minute}`;
+  });
+};
+
+const limitMinute = ({ diagramArr }: { diagramArr: DiagramArr }) => {
+  const currentMinutes =
+    new Date().getHours() * 3600 +
+    new Date().getMinutes() * 60 +
+    new Date().getSeconds();
+  return diagramArr.map((diagram: number) => {
+    const diagramMinutes =
+      new Date(diagram).getHours() * 3600 +
+      new Date(diagram).getMinutes() * 60 +
+      new Date(diagram).getSeconds();
+    return diagramMinutes - currentMinutes;
+  });
+};
